d640e8bc6a8e83751ddaa76266ede42f9b34639b
Dan <22007659+dansenpir@users.noreply.github.com>
2025-08-30 22:23:09 -0300

    chore: initializes spring project

---
+++ src/main/java/com/synapse/money/SynapseMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.synapse.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapseMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapseMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/synapse/money/SynapseMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synapse.money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
