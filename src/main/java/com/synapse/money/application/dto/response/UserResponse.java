57237ac466ea932fe4b42ebe967e477e2161d8f2
Dan <22007659+senegalha@users.noreply.github.com>
2025-10-28 10:38:28 -0300

    feat(application): create customized UserResponse DTO

---
+++ src/main/java/com/synapse/money/application/dto/response/UserResponse.java
@@ -0,0 +1,21 @@
+package com.synapse.money.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime createdAt;
+}
