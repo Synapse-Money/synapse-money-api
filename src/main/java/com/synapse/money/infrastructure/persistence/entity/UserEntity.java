ea362a1a61402039e5709081a61a58f6acd4a97c
Dan <22007659+senegalha@users.noreply.github.com>
2025-10-28 19:56:24 -0300

    feat(infrastructure): add @Setter to UserEntity for JPA operations

--- src/main/java/com/synapse/money/infrastructure/persistence/entity/UserEntity.java
+++ src/main/java/com/synapse/money/infrastructure/persistence/entity/UserEntity.java
@@ -13,11 +13,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
+@Setter
 @Getter
 @Builder
 @NoArgsConstructor
