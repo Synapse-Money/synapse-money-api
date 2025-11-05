15596e35640dd44a2bf252b105ac313192575a7f
Dan <22007659+senegalha@users.noreply.github.com>
2025-11-05 19:46:28 -0300

    test(domain): ensure category is saved successfully

---
+++ src/test/java/com/synapse/money/domain/repository/CategoryRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.synapse.money.domain.repository;
+
+import com.synapse.money.domain.entity.Category;
+import com.synapse.money.domain.entity.User;
+import com.synapse.money.domain.enums.ETransactionType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Category Repository Tests")
+public class CategoryRepositoryTest {
+
+    CategoryRepository categoryRepository;
+
+    @Test
+    void shouldSaveCategorySuccessfully() {
+        User user = User.builder()
+                .id(1L)
+                .email("john.doe@example.com")
+                .password("hashedPassword123")
+                .firstName("John")
+                .lastName("Doe")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        Category category = Category.builder()
+                .id(1L)
+                .user(user)
+                .name("Test Category")
+                .color("#333333")
+                .icon("Test Icon")
+                .transactionType(ETransactionType.INCOME)
+                .isDefault(true)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        Category categorySaved = category.save(category);
+
+        assertThat(categorySaved).isNotNull();
+        assertThat(categorySaved.getId()).isNotNull();
+    }
+}
